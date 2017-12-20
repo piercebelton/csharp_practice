534b6826f5e5be379e19c9d9fc197a163184cf1c
Pierce Belton <pierce.belton@bydeluxe.com>
2017-12-20 10:37:27 -0800

    initial commit, more csharp practice

---
+++ csharp_practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_practice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var names = new List<string> { "Melissa", "Pierce", "Jethro" };
+            names.Add("Isaac");
+            names.Remove("Jethro");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
