12dbe6438c6fd268e59a6733bff8d223034a0992
Pierce Belton <pierce.belton@bydeluxe.com>
2017-12-20 11:31:33 -0800

    completed quickstart collections guide from microsoft docs

--- csharp_practice/Program.cs
+++ csharp_practice/Program.cs
@@ -7,6 +7,25 @@ namespace csharp_practice
     {
         public static void Main(string[] args)
         {
+            // WorkingWithStringArrays();
+            List<int> fibonacciNumbers = new List<int> { 1, 1 };
+
+
+            while(fibonacciNumbers.Count < 20)
+            {
+				int previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+				int previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+				fibonacciNumbers.Add(previous + previous2);            
+            }
+
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void WorkingWithStringArrays()
+        {
             var names = new List<string> { "Melissa", "Pierce", "Jethro" };
             names.Add("Isaac");
             names.Remove("Jethro");
@@ -14,6 +33,25 @@ namespace csharp_practice
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+            Console.WriteLine("My name is {0}", names[1]);
+            Console.WriteLine("I have added {0} and {1} to the list.", names[0], names[2]);
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            int index = names.IndexOf("Pierce");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+			}
         }
     }
 }
