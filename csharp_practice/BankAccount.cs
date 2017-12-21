0dfef9eb9a6a45e4833bdf27ae8ad0e5160c5d5d
Pierce Belton <pierce.belton@bydeluxe.com>
2017-12-20 16:01:01 -0800

    finished class tutorials from microsoft docs

--- csharp_practice/BankAccount.cs
+++ csharp_practice/BankAccount.cs
@@ -1,30 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+
+
 namespace csharp_practice
 {
     public class BankAccount
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                    balance += item.Amount;
+                return balance;
+            }
+        }
 
         private static int accountNumberSeed = 0000000001;
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-            
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
-        public void MakeWithdrawl(decimal amount, DateTime date, string payee, string note)
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
         {
-            
+			if (amount <= 0)
+				throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+			if (Balance - amount < 0)
+				throw new InvalidOperationException("Not sufficient funds for this withdrawl");
+			var withdrawl = new Transaction(-amount, date, note);
+			allTransactions.Add(withdrawl);
         }
 
         public BankAccount(string name, decimal initialBalance)
         {
-            this.Owner = name;
-            this.Balance = initialBalance;
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+			
+            this.Owner = name;
+        
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+
+            return report.ToString();
         }
     }
 }
--- csharp_practice/Program.cs
+++ csharp_practice/Program.cs
@@ -11,6 +11,32 @@ namespace csharp_practice
             // FibonacciNumbers();
             var account = new BankAccount("Pierce", 8000000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        
+            account.MakeWithdrawl(1950, DateTime.Now, "Rent Payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(3000, DateTime.Now, "Got Paid");
+            Console.WriteLine(account.Balance);
+
+            // Test that initial balance must be positive
+            //try {
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //} catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            //// Test for a negative balance
+            //try {
+            //    account.MakeWithdrawl(9000000, DateTime.Now, "Attempt to overdraw");
+            //} catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            Console.WriteLine(account.GetAccountHistory());
+        
         }
 
         public static void FibonacciNumbers()
